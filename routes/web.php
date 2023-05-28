8e109d3fcaba1bb961f605ba6ffc70545f8f5e63
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-05-28 18:37:56 +0900

    components から primary-button を作成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/test', [TestController::class, 'test'])
+    ->name('test');
