f268e16bbbdf618ce7fe2aa2a48fafa99385c460
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-05-30 18:22:40 +0900

    フォームからデータを保存
    # what
    - フォーム情報を保存するために、データベースと連結
    - セッションを活用して、保存されたことを表示
    # why
    - 情報を保存し、次回表示させるため
    - 保存できたことがわかるようにするため

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,6 @@ Route::get('/test', [TestController::class, 'test'])
     ->name('test');
 
 Route::get('post/create', [PostController::class, 'create']);
+
+Route::post('post', [PostController::class, 'store'])
+    ->name('post.store');
