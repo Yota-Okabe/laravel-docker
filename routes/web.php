fd9bf8186d2a96a602c4d91124d6169d27e0739f
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-06-01 22:18:37 +0900

    投稿データの一覧表示

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@ Route::get('post/create', [PostController::class, 'create']);
 
 Route::post('post', [PostController::class, 'store'])
     ->name('post.store');
+
+Route::get('post', [PostController::class, 'index']);
